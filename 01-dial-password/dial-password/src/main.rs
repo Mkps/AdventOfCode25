c34f65fb95cdc552b892bb3c782cc3e9688e0590
Alexis <aloubier@student.42.fr>
2025-12-01 23:52:16 +0100

    Finished challenge2

--- 01-dial-password/dial-password/src/main.rs
+++ 01-dial-password/dial-password/src/main.rs
@@ -1,19 +1,30 @@
 fn main() {
-    let mut current_position = 50;
+    let mut current_position: i32 = 50;
     let input = std::fs::read_to_string("input.txt");
-    let mut password_count = 0;
-    for line in input.expect("REASON").lines() {
+    let mut password_count: u32 = 0;
+
+    for line in input.expect("Could not open input.txt").lines() {
         let dir = line.chars().next().unwrap();
-        let mut num: i32 = line[1..].parse().unwrap();
-        // println!("{}", dir);
-        // println!("{}", num);
-        if dir == 'L' {
-            num *= -1;
-        } 
-        current_position = (current_position + num % 100 + 100) % 100;
-        if current_position == 0 {
-            password_count += 1;
+        let num: i32 = line[1..].parse().unwrap();
+
+        let rot = if dir == 'L' { 
+            -num
+        } else {
+            num
+        };
+
+        let sum = current_position + rot;
+
+        // If we actually looped from the left
+        if dir == 'L'  && sum <= 0{
+            // Avoid double counting when we're already on 0
+            password_count += ((sum / 100).abs() + 1 - (current_position == 0) as i32) as u32;
         }
+        // Or from the right
+        else if sum >= 100 {
+            password_count += (sum / 100) as u32;
+        } 
+        current_position = (current_position + rot % 100 + 100) % 100;
     }
-    println!("Password count is : {}", password_count)
+    println!("Number of time we hit 0: {} times.", password_count)
 }
