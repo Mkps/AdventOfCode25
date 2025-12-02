b867c8273d711eab8a2ad6ec50e7a61d4f185e1d
Alexis <aloubier@student.42.fr>
2025-12-02 22:01:29 +0100

    Finished challenge2

--- 02-gift-shop/gift-shop/src/main.rs
+++ 02-gift-shop/gift-shop/src/main.rs
@@ -1,8 +1,40 @@
+fn is_repeated_pattern(s: &str) -> bool {
+    if s.len() <= 1 {
+        return false;
+    }
 
+    let doubled = format!("{s}{s}");
+    let inner = &doubled[1..doubled.len() - 1];
+    inner.contains(s)
+}
+
+fn check_these_repeating_digits(start: u64, end: u64) -> u64 {
+    let mut ret = 0;
+
+    for n in start..=end {
+        let s = n.to_string();
+        if is_repeated_pattern(&s){
+            ret += n;
+        }
+    }
+    // println!("Ret is {}", ret);
+    ret
+}
 
 fn challenge2() {
-    println!("Not implemented!");
-
+    let raw = std::fs::read_to_string("example.txt");
+    let mut answer: u64 = 0;
+    let mut input = raw.expect("Could not split input.txt");
+    input = input.trim().to_string(); //Triming ending newline or we get a panic
+    let values = input.split(",");
+    for pair in values {
+        // println!("Value is {}", pair);
+        let mut iter = pair.split("-");
+        let a = iter.next().unwrap().parse::<u64>().unwrap();
+        let b = iter.next().unwrap().parse::<u64>().unwrap();
+        answer += check_these_repeating_digits(a, b);
+    }
+    println!("Answer is {}", answer);
 }
 
 fn check_these_dubs(start: u64, end: u64) -> u64 {
@@ -20,7 +52,7 @@ fn check_these_dubs(start: u64, end: u64) -> u64 {
             }
         }
     }
-    println!("Ret is {}", ret);
+    // println!("Ret is {}", ret);
     ret
 }
 
@@ -31,12 +63,11 @@ fn challenge1() {
     input = input.trim().to_string(); //Triming ending newline or we get a panic
     let values = input.split(",");
     for pair in values {
-        println!("Value is {}", pair);
+        // println!("Value is {}", pair);
         let mut iter = pair.split("-");
         let a = iter.next().unwrap().parse::<u64>().unwrap();
         let b = iter.next().unwrap().parse::<u64>().unwrap();
         answer += check_these_dubs(a, b);
-        // println!("Start is {} end is {}", a, b);
     }
     println!("Answer is {}", answer);
 }
